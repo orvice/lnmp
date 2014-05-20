fecd6199502f41c4ab560273a7cbadfe4e8fb538
orvice <orvice@gmail.com>
2014-05-20 12:57:46 +0800

    update

--- debian7.sh
+++ debian7.sh
@@ -3,13 +3,9 @@
 #https://github.com/orvice/lnmp
 #@orvice
 # htpp://orvice.org
-# last update 2014-5-18
+# last update 2014-5-20
 
-#Add Dotdeb
-echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
-echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
-wget http://www.dotdeb.org/dotdeb.gpg 
-apt-key add  dotdeb.gpg
+
 
 
 #Add MariaDB
@@ -21,13 +17,25 @@ add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/debian wheezy
 #Update
 apt-get update
 apt-get upgrade
+
+#Install MariaDB
+apt-get install mariadb-server -y
+
+#Add Dotdeb
+echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
+echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
+wget http://www.dotdeb.org/dotdeb.gpg 
+apt-key add  dotdeb.gpg
+
+#Update
+apt-get update
+apt-get upgrade
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker  
 
 #Install Nginx
 apt-get install -y nginx-full -y
 
-#Install MariaDB
-apt-get install mariadb-server -y
+
 
 #Install PHP 
 apt-get install php5-fpm php5-gd php5-mysql php5-memcache php5-curl memcached -y
