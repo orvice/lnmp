62907d8d1a5a5fd05d4bdd474283cd13b1c070f8
orvice <orvice@gmail.com>
2014-09-20 14:47:39 +0800

    update nginx

--- debian7.sh
+++ debian7.sh
@@ -9,7 +9,7 @@
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script "
+    echo "Error: You must  run this script as root"
     exit 1
 fi
 
@@ -30,19 +30,19 @@ apt-get install mariadb-server -y
 echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
 echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
 cd /tmp
-wget http://www.dotdeb.org/dotdeb.gpg 
+wget http://www.dotdeb.org/dotdeb.gpg
 apt-key add  dotdeb.gpg
 
 #Update
 apt-get update
 
 #Remove Apache
-apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker  
+apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker
 
 #Install Nginx
 apt-get install -y nginx-full -y
 
-#Install PHP 
+#Install PHP
 apt-get install php5-fpm php5-gd php5-mysql php5-memcache php5-curl memcached -y
 
 #Start Nginx
@@ -58,3 +58,15 @@ useradd -s /sbin/nologin -M -g mysql mysql
 mkdir /homw/www
 mkdir /home/www/default
 chown www:www /home/www -R
+
+#Set Permission
+chown www:www /var/lib/nginx/fastcgi -R
+chown www:www /home/www -R
+
+#Edit Nginx
+cd /etc/nginx
+rm nginx.conf
+mkdir vhost
+wget http://img.orx.me/lnmp/nginx/nginx.conf
+cd /etc/nginx/vhost
+wget http://img.orx.me/lnmp/nginx/vhost/default.conf
